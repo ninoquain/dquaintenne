30fe765a987c5039de45a7fb69a261f5da89345d
Dominique Quaintenne <dominique.quaintenne@gmail.com>
2020-10-19 18:13:55 +0200

    Modification du fichier elemsphp nav
    Modification de navigation des pages

--- elementsphp/nav.php
+++ elementsphp/nav.php
@@ -3,11 +3,11 @@
         <label for="mobile"><img src="assets/icones/burger-menu.png" alt="icone du menu déroulant"></label>
         <input type="checkbox" id="mobile" role="button">
        <ul>
-            <li><a href="http://localhost/dominique-quaintenne/accueil" class="largeScreen">Accueil</a></li>
-            <li><a href="http://localhost/dominique-quaintenne/développement-web" class="largeScreen">Developpement Web</a></li>
-            <li><a href="http://localhost/dominique-quaintenne/web-design" class="largeScreen">Web Design</a></li>
-            <li><a href="http://localhost/dominique-quaintenne/optimisation-seo" class="largeScreen">Optimisation SEO</a></li>
-            <li><a href="http://localhost/dominique-quaintenne/passions" class="largeScreen">Passions</a></li>
+            <li><a href="http://dquaintenne/accueil" class="largeScreen">Accueil</a></li>
+            <li><a href="http://dquaintenne/développement-web" class="largeScreen">Developpement Web</a></li>
+            <li><a href="http://dquaintenne/web-design" class="largeScreen">Web Design</a></li>
+            <li><a href="http://dquaintenne/optimisation-seo" class="largeScreen">Optimisation SEO</a></li>
+            <li><a href="http://dquaintenne/passions" class="largeScreen">Passions</a></li>
         </ul> 
     </div>
     
